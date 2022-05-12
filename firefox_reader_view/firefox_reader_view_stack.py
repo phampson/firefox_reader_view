53436505e41b72f454c22612df949e32db290bda
phampson <parker.hampson@gmail.com>
2022-05-11 22:11:40 -0700

    Cleaning up the stack definition

--- firefox_reader_view/firefox_reader_view_stack.py
+++ firefox_reader_view/firefox_reader_view_stack.py
@@ -2,9 +2,6 @@ from constructs import Construct
 from aws_cdk import (
     Duration,
     Stack,
-    aws_sqs as sqs,
-    aws_sns as sns,
-    aws_sns_subscriptions as subs,
     aws_lambda as _lambda,
     aws_apigateway as apigw,
 )
@@ -21,6 +18,8 @@ class FirefoxReaderViewStack(Stack):
             runtime=_lambda.Runtime.NODEJS_14_X,
             code=_lambda.Code.from_asset('lambda'),
             handler='hello.handler',
+            memory_size=4096,
+            timeout=Duration.seconds(60),
         )
 
         # Adding in a gateway to route the lambda
@@ -28,15 +27,3 @@ class FirefoxReaderViewStack(Stack):
             self, 'Endpoint',
             handler=my_lambda,
         )
-
-        # I don't think I need this so let's find out lol
-        # queue = sqs.Queue(
-        #     self, "FirefoxReaderViewQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
-        #
-        # topic = sns.Topic(
-        #     self, "FirefoxReaderViewTopic"
-        # )
-        #
-        # topic.add_subscription(subs.SqsSubscription(queue))
