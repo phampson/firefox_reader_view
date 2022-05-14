d087a022f1348b000b34754e0ce4873cc29b244e
phampson <parker.hampson@gmail.com>
2022-05-13 19:04:07 -0700

    adding in a pipeline template

--- firefox_reader_view/firefox_reader_view_stack.py
+++ firefox_reader_view/firefox_reader_view_stack.py
@@ -4,11 +4,11 @@ from aws_cdk import (
     Stack,
     aws_lambda as _lambda,
     aws_apigateway as apigw,
+    pipelines
 )
 
 
 class FirefoxReaderViewStack(Stack):
-
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
