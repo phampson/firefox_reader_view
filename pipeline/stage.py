d2b062b29c69f0d53912fedaf86a1736779205fe
phampson <parker.hampson@gmail.com>
2022-05-14 00:28:35 -0700

    Fixing pipeline error

--- pipeline/stage.py
+++ pipeline/stage.py
@@ -5,6 +5,6 @@ from firefox_reader_view.firefox_reader_view_stack import FirefoxReaderViewStack
 
 class PipelineStage(Stage):
     def __init__(self, scope, stage_name, props) -> None:
-        super.__init__(scope, stage_name, props)
+        super().__init__(scope, stage_name)
 
         lambda_stack = FirefoxReaderViewStack(self, "FirefoxReaderViewStack", stage_name)
