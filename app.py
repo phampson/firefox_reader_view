ddb98f0ed8afb00413c7807f6c69af3626aa4f14
phampson <parker.hampson@gmail.com>
2022-05-13 22:56:32 -0700

    Adding in the region and amazon account id

--- app.py
+++ app.py
@@ -3,9 +3,15 @@
 import aws_cdk as cdk
 
 from firefox_reader_view.firefox_reader_view_stack import FirefoxReaderViewStack
-
+from pipeline.pipeline_stack import PipelineStack
 
 app = cdk.App()
-FirefoxReaderViewStack(app, "firefox-reader-view")
+
+# FirefoxReaderViewStack(app, "firefox-reader-view")
+PipelineStack(
+    app,
+    "PipelineStack",
+    env=cdk.Environment(account="653592191043", region="us-west-1")
+)
 
 app.synth()
--- pipeline/pipeline_stack.py
+++ pipeline/pipeline_stack.py
@@ -3,7 +3,7 @@ from aws_cdk.pipelines import CodePipeline, CodePipelineSource, ShellStep
 from constructs import Construct
 
 
-class MyPipelineStack(cdk.Stack):
+class PipelineStack(cdk.Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
